ccb11109256ce7ece68091ffa5ac1299b80403fb
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2023-01-18 17:28:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Status="";
+video = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+function draw(){
+    image(video,0,0,480,380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
